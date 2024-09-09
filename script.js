f70c4c889996fc900ccbe7e89696a866d2b88527
luancvb <luancaiquelcvb@gmail.com>
2024-09-09 13:17:02 -0300

    Alterações para tocar o video

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function abrirVideo() {
-    const videoElement = document.querySelector('.video'); // Seletor da classe do vídeo
-    videoElement.style.display = 'block'; // Exibe o vídeo
-    videoElement.play(); // Inicia a reprodução automaticamente
-}
 
